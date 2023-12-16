44645a68769d326273a375f9285a5f606353f6a9
mahirsuljic-fet <mahir.suljic@fet.ba>
2023-12-16 16:45:43 +0100

    usefulminifuncs-example.cpp edit

--- usefulminifuncs-example.cpp
+++ usefulminifuncs-example.cpp
@@ -13,13 +13,14 @@ int main(int argc, char* argv[])
   std::list<int> l = random_list_int(10);
   std::map<std::string, int> m = ordered_map_string_int(10);
   int* arr_d = random_array_int(5, 1, 10);
-  int arr_s[5] = { 1, 2, 3, 4, 5 };
+  int arr_s[] = { 1, 2, 3, 4, 5 };
 
   print_container(v);
   print_container(l);
   print_container(m);
   print_container(arr_d, 5);
   print_container(arr_d, arr_d + 5);
+  print_container(arr_s);
   print_container(arr_s, 5);
   print_container(arr_s, arr_s + 5);
   print_container(std::begin(arr_s), std::end(arr_s));
