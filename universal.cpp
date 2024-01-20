3f034b6aa6368422f3ba946c22d2b9800864940a
mahirsuljic-fet <mahir.suljic@fet.ba>
2024-01-20 22:38:24 +0100

    Language fix

--- universal.cpp
+++ universal.cpp
@@ -1,117 +1,23 @@
 #include "usefulminifuncs.cpp"
 #include <algorithm>
-#include <cstddef>
-#include <initializer_list>
+#include <functional>
 #include <iostream>
-
-template <typename T>
-class Foo
-{
-  private:
-    T* _p;
-    size_t _size;
-
-  public:
-    // konstruktori i destruktor
-    Foo(const std::initializer_list<T>&);
-    template <typename U>
-    Foo(const std::initializer_list<U>&);
-    ~Foo() { delete[] _p; }
-
-    // problematicni operator
-    template <typename U>
-    Foo<T>& operator=(Foo<U>&&);
-
-    // pomocne metode
-    void print(std::ostream& os = std::cout) const;
-    size_t size() const { return _size; }
-    T& front() const { return _p[0]; }
-    void clean()
-    {
-      _p = nullptr;
-      _size = 0;
-    }
-};
-
-template <typename T>
-Foo<T>::Foo(const std::initializer_list<T>& list)
-{
-  std::cout << "IC - T\n";
-
-  _p = new T[list.size()];
-  _size = list.size();
-
-  T* it = _p;
-  for (auto& el : list)
-    *it++ = el;
-}
-
-template <typename T>
-template <typename U>
-Foo<T>::Foo(const std::initializer_list<U>& list)
-{
-  std::cout << "IC - TU\n";
-
-  _p = new T[list.size()];
-  _size = list.size();
-
-  T* it = _p;
-  for (auto& el : list)
-    *it++ = el;
-}
-
-template <typename T>
-void Foo<T>::print(std::ostream& os) const
-{
-  for (auto i = 0; i < _size; ++i)
-    os << _p[i] << " ";
-  os << std::endl;
-}
-
-//========================================================================================
-
-template <typename T>
-template <typename U>
-Foo<T>& Foo<T>::operator=(Foo<U>&& other)
-{
-  delete[] _p;
-
-  // _p = other._p;
-  // _p = (T*)other._p;
-
-  // PITANJE IZ OVOG PRIMJERA
-  //
-  // Da li je moguce napraviti ovakav move operator= kada imamo razlicite tipove pointera?
-
-  // GLAVNO PITANJE
-  //
-  // Da li je uopce pametno raditi konverziju tipova kada radimo sa pointerima,
-  // i ako da, koji je pravilan nacin da se to odradi?
-
-  _p = (T*)&other.front(); // kompajlat ce ali ne valja jer bukvalno preuzme bajtove
-  _size = other.size();
-
-  other.clean();
-
-  return *this;
-}
-
-//========================================================================================
+#include <string>
+#include <type_traits>
 
 int main(int argc, char* argv[])
 {
-  Foo<int> i = { 1, 2, 3 };
-  Foo<double> d = { 4, 5, 6 };
+  int i = 0;
+  int& i_l = i;
+  const int& i_cl = 0;
+  int&& i_r = 0;
+  const int&& i_cr = 0;
 
-  i.print();
-  d.print();
-
-  d = std::move(i);
-  // ili
-  // i = std::move(d);
-
-  i.print();
-  d.print();
+  std::cout << std::is_same<decltype(std::move(i)), int&&>::value << std::endl
+            << std::is_same<decltype(std::move(i_l)), int&&>::value << std::endl
+            << std::is_same<decltype(std::move(i_cl)), const int&&>::value << std::endl
+            << std::is_same<decltype(std::move(i_r)), int&&>::value << std::endl
+            << std::is_same<decltype(std::move(i_cr)), const int&&>::value << std::endl;
 
   return 0;
 }
